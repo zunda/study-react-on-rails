63a3fe3850d8acd09ab7766174972d9ffcc24a6f
zunda <zundan@gmail.com>
2015-11-11 12:48:57 -1000

    Show comments

--- app/assets/javascripts/components/sample.js.jsx
+++ app/assets/javascripts/components/sample.js.jsx
@@ -14,7 +14,8 @@ var CommentList = React.createClass({
   render: function() {
     return (
       <div className="commentList">
-        Hello, world! I am a CommentList.
+        <Comment author="Pete Hunt">This is first comment</Comment>
+        <Comment author="John Walke">This is second comment</Comment>
       </div>
     );
   }
@@ -25,6 +26,19 @@ var CommentForm = React.createClass({
     return (
       <div className="commentForm">
         Hello, world! I am a CommentForm.
+      </div>
+    );
+  }
+});
+
+var Comment = React.createClass({
+  render: function() {
+    return (
+      <div className="comment">
+        <h3 className="commentAuthor">
+          {this.props.author}
+        </h3>
+        {this.props.children}
       </div>
     );
   }
