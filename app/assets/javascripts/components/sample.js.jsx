687f09ea05f48a5a9b66978fbbd385aa9a5e4ce4
zunda <zundan@gmail.com>
2015-11-11 12:44:16 -1000

    Include CommentList and CommentForm into  CommentBox

--- app/assets/javascripts/components/sample.js.jsx
+++ app/assets/javascripts/components/sample.js.jsx
@@ -2,7 +2,29 @@ var CommentBox = React.createClass({
   render: function() {
     return (
       <div className="commentBox">
-        Hello, world! I am a CommentBox.
+        <h2>Comments</h2>
+	<CommentList />
+	<CommentForm />
+      </div>
+    );
+  }
+});
+
+var CommentList = React.createClass({
+  render: function() {
+    return (
+      <div className="commentList">
+        Hello, world! I am a CommentList.
+      </div>
+    );
+  }
+});
+
+var CommentForm = React.createClass({
+  render: function() {
+    return (
+      <div className="commentForm">
+        Hello, world! I am a CommentForm.
       </div>
     );
   }
