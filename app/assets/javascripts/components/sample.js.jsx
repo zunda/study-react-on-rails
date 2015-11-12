7785f8a59cb8e692c5834494d4828db35f53fdf2
zunda <zundan@gmail.com>
2015-11-11 19:03:28 -1000

    Use dangerouslySetInnerHTML

--- app/assets/javascripts/components/sample.js.jsx
+++ app/assets/javascripts/components/sample.js.jsx
@@ -32,13 +32,18 @@ var CommentForm = React.createClass({
 });
 
 var Comment = React.createClass({
+  rawMarkup: function() {
+    var rawMarkup = marked(this.props.children.toString(), {sanitize: true});
+    return { __html: rawMarkup };
+  },
+
   render: function() {
     return (
       <div className="comment">
         <h3 className="commentAuthor">
           {this.props.author}
         </h3>
-        {marked(this.props.children.toString())}
+        <span dangerouslySetInnerHTML={this.rawMarkup()} />
       </div>
     );
   }
