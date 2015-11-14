1eede7b34397eb3ef7397bc4356f1141001a9e22
zunda <zundan@gmail.com>
2015-11-13 19:03:46 -1000

    Obtain Comments through AJAX

--- app/assets/javascripts/components/sample.js.jsx
+++ app/assets/javascripts/components/sample.js.jsx
@@ -1,14 +1,25 @@
-var data = [
-  {id:1, author: "Pete Hunt", text: "This is one comment"},
-  {id:2, author: "Jordan Walke", text: "This is *another* comment"}
-];
-
 var CommentBox = React.createClass({
+  getInitialState: function() {
+    return {data: []};
+  },
+  componentDidMount: function() {
+    $.ajax({
+      url: this.props.url,
+      dataType: 'json',
+      cache: false,
+      success: function(data) {
+        this.setState({data: data});
+      }.bind(this),
+      error: function(xhr, status, err) {
+        console.error(tihs.props.url, status, err.toString());
+      }.bind(this)
+    });
+  },
   render: function() {
     return (
       <div className="commentBox">
         <h2>Comments</h2>
-        <CommentList data={data} />
+        <CommentList data={this.state.data} />
         <CommentForm />
       </div>
     );
