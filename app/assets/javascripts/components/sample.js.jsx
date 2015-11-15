537db293e5ec8fea6451a84511e93d00c5815c25
zunda <zundan@gmail.com>
2015-11-14 14:25:16 -1000

    Submit comment to the server

--- app/assets/javascripts/components/sample.js.jsx
+++ app/assets/javascripts/components/sample.js.jsx
@@ -13,7 +13,18 @@ var CommentBox = React.createClass({
     });
   },
   handleCommentSubmit: function(comment) {
-    // TODO: submit to the sever and refresh the list
+    $.ajax({
+      url: this.props.url,
+      dataType: 'json',
+      type: 'POST',
+      data: { 'comment': comment },
+      success: function(data) {
+        this.setState({data: data});
+      }.bind(this),
+      error: function(xhr, status, err) {
+        console.error(this.props.url, status. err.toString());
+      }.bind(this)
+    });
   },
   getInitialState: function() {
     return {data: []};
