84ed5bc8f2259dec4c659805281fd5041306206f
zunda <zundan@gmail.com>
2015-11-14 14:27:57 -1000

    Update optimistically

--- app/assets/javascripts/components/sample.js.jsx
+++ app/assets/javascripts/components/sample.js.jsx
@@ -13,6 +13,10 @@ var CommentBox = React.createClass({
     });
   },
   handleCommentSubmit: function(comment) {
+    var comments = this.state.data;
+    comment.id = Date.now();
+    var newComments = comments.concat([comment]);
+    this.setState({data: newComments});
     $.ajax({
       url: this.props.url,
       dataType: 'json',
